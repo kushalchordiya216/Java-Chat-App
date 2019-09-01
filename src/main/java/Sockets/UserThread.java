52586860ad66a0bc32cad524aff924a1f8db8fe2
Kushal Chordiya <chordiyakushal@gmail.com>
2019-09-01 18:41:42 +0530

    Basic CLI implementation
    Allows for communication between multiple clients in non-blocking I/O. Currently limit set to 10 clients.

---
+++ src/main/java/Sockets/UserThread.java
@@ -0,0 +1,69 @@
+package Sockets;
+
+import java.io.*;
+import java.net.*;
+
+public class UserThread implements Runnable {
+    private Socket socket;
+    private Server server;
+    private PrintWriter writer;
+
+    public UserThread(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    public void run() {
+        try {
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+
+            printUsers();
+
+            String userName = reader.readLine();
+            server.addUserName(userName);
+
+            String serverMessage = "New user connected: " + userName;
+            server.broadcast(serverMessage, this);
+
+            String clientMessage;
+
+            do {
+                clientMessage = reader.readLine();
+                serverMessage = "[" + userName + "]: " + clientMessage;
+                server.broadcast(serverMessage, this);
+            } while (!clientMessage.equals("bye"));
+
+            server.removeUser(userName, this);
+            socket.close();
+
+            serverMessage = userName + " has quitted.";
+            server.broadcast(serverMessage, this);
+
+        } catch (IOException ex) {
+            System.out.println("Error in UserThread: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Sends a list of online users to the newly connected user.
+     */
+    private void printUsers() {
+        if (server.hasUsers()) {
+            writer.println("Connected users: " + server.getUserNames());
+        } else {
+            writer.println("No other users connected");
+        }
+    }
+
+    /**
+     * Sends a message to the client.
+     */
+    void sendMessage(String message) {
+       writer.println(message);
+    }
+}
